047046b11382eddedd16f33885960a71301d950a
Ratodo <34605543+Ratodo@users.noreply.github.com>
2020-03-21 21:22:58 +0800

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const config = {
-    ENDPOINT: "https://v1.nocodeapi.com/ratodo/uptime/YMnYEKpjxRtZbKET?monitors=784567220-781549242-784567241",
+    ENDPOINT: "https://v1.nocodeapi.com/ratodo/uptime/YMnYEKpjxRtZbKET?monitors=784567198-781549242-784567241",
 
     title: "Ratodo",
 
